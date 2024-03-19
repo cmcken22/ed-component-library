b6823dc98dac07237d3d32ee919aab1998208f2d
Conner McKenna <conner.mckenna@lockdocs.com>
2024-03-18 22:07:50 -0400

    fix: fixing build for sb and npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,55 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
 import removeConsole from "vite-plugin-remove-console";
 
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  console.log("\n\n------------");
+  console.log("env:", env.BUILD_MODE);
+  console.log("------------\n\n");
+
+  const plugins: any[] = [
     react(),
-    removeConsole(),
     libInjectCss(),
     dts({
       include: ["src"],
       exclude: ["**/*.stories.tsx"],
     }),
-  ],
-  build: {
-    copyPublicDir: false,
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es", "cjs"],
-      fileName: (format) => `index.${format}.js`,
+  ];
+  // the build for storybook doesn't like the removeConsole plugin
+  // so only add it for lib build
+  if (env.BUILD_MODE === "lib") {
+    plugins.push(removeConsole());
+  }
+
+  return {
+    plugins,
+    build: {
+      copyPublicDir: false,
+      lib: {
+        entry: resolve(__dirname, "src/index.ts"),
+        formats: ["es", "cjs"],
+        fileName: (format) => `index.${format}.js`,
+      },
+      rollupOptions: {
+        external: [
+          "react",
+          "react/jsx-runtime",
+          /^react\//,
+          "@mui/material",
+          "@emotion/react",
+          "@emotion/styled",
+        ],
+      },
     },
-    rollupOptions: {
-      external: [
-        "react",
-        "react/jsx-runtime",
-        /^react\//,
-        "@mui/material",
-        "@emotion/react",
-        "@emotion/styled",
-      ],
+    resolve: {
+      alias: {
+        src: resolve(__dirname, "./src"),
+        "test-utils": resolve(__dirname, "./utils/test-utils"),
+      },
     },
-  },
-  resolve: {
-    alias: {
-      src: resolve(__dirname, "./src"),
-      "test-utils": resolve(__dirname, "./utils/test-utils"),
-    },
-  },
+  };
 });

5ef4e5305be54cf21dc8dbfb6cfe74fe785b9252
Conner McKenna <conner.mckenna@lockdocs.com>
2024-04-15 01:01:17 -0400

    Updates

---
+++ assets/RangePickerCalendar.stories-DVu48fYl.js
@@ -0,0 +1,17 @@
+import{j as d}from"./jsx-runtime-Du8NFWEI.js";import{f}from"./index-BnUgH7jw.js";import{s as D}from"./index-B_pG5fU1.js";import{b as i}from"./Button-Dq2JyfON.js";import"./index-Dl6G-zuu.js";import"./index-D5lE8zFj.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-Bpxk95mn.js";import"./createTheme-DtgTEXL7.js";import"./Icon.types-DhqP0w5k.js";import"./Typography-BGHga1vv.js";import"./index-Sf89G3JB.js";import"./useTheme-D-YGWJ_P.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./palette-CiKahY_l.js";import"./useOnHover-DcG81d-b.js";import"./Box-TRhWXm-4.js";import"./testIds-Bl6124vV.js";import"./ThemeWrapper-DNKQPsvA.js";import"./Typography-C2npGzRJ.js";import"./Typography-CoWQAh3d.js";import"./index-D1_ZHIBm.js";import"./index-CpmRWZ9e.js";import"./GlobalStyles-D5B41vK4.js";import"./Avatar-_rNZu4pO.js";import"./Tooltip-Dq2jsA7A.js";import"./TransitionGroupContext-HZ0X4aoe.js";import"./inheritsLoose-B7h9gheN.js";import"./DatePicker-CyootzQZ.js";import"./index-Beu9b4Vr.js";import"./createSvgIcon-Dq_AaE_0.js";import"./ButtonBase-B8BUVujF.js";import"./assertThisInitialized-B9jnkVVz.js";import"./useCommonOnChangeHandler-MKW6boOs.js";import"./useFormControl-EHw8eS4L.js";import"./Paper-BfG0c4km.js";import"./Checkbox-CEycOWC4.js";import"./FormControlLabel-Bi0SKVm3.js";import"./styled-3drYQxig.js";import"./Chip-BmMAu8xS.js";import"./DateField-CcM_in6z.js";import"./DateRangeField-B5h_opQz.js";import"./Grid-Bu2osdGr.js";import"./Input-CIEKK-yg.js";import"./TextArea-VzZFGWTb.js";const me={title:"Example/Date Picker/RangePickerCalendar",component:i,parameters:{layout:"centered",docs:{source:{transform:D("RangePickerCalendar")}}},tags:["autodocs"],argTypes:{onSelect:{action:"onChange"}},render:e=>{let t={};if(e!=null&&e.currentDate&&(t={...t,currentDate:new Date(e==null?void 0:e.currentDate)}),e!=null&&e.value&&Array.isArray(e==null?void 0:e.value)){const u=e==null?void 0:e.value.map(c=>new Date(c));t={...t,value:u}}return d.jsx(i,{...e,...t})}},r={args:{...i.defaultProps,disableFuture:!1,disableCurrent:!1,disablePast:!1,currentDate:new Date,numberOfMonths:2,onSelect:f()}},o={args:{...r.args,tools:!0,previewSelection:!0}};var a,m,n;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    ...RangePickerCalendar.defaultProps,
+    disableFuture: false,
+    disableCurrent: false,
+    disablePast: false,
+    currentDate: new Date(),
+    numberOfMonths: 2,
+    onSelect: fn()
+  }
+}`,...(n=(m=r.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};var p,s,l;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    tools: true,
+    previewSelection: true
+  }
+}`,...(l=(s=o.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};const ne=["Default","WithTools"];export{r as Default,o as WithTools,ne as __namedExportsOrder,me as default};

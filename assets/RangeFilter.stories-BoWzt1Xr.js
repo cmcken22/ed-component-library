58487fe2b088c2f20cdef5a5622a5da1b1d95e85
Conner McKenna <conner.mckenna@lockdocs.com>
2024-04-24 14:19:13 -0400

    Updates

---
+++ assets/RangeFilter.stories-BoWzt1Xr.js
@@ -0,0 +1,30 @@
+import{c as g}from"./commonArgTypes-Cz1rbySM.js";import{s as c}from"./index-B_pG5fU1.js";import{k as o,C as u}from"./Button-Cv69wKt2.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./index-DFf-bhEl.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-Bpxk95mn.js";import"./createTheme-DtgTEXL7.js";import"./Icon.types-Ba5UCeQa.js";import"./Typography-BGHga1vv.js";import"./index-_OcG2YHj.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./palette-CiKahY_l.js";import"./useOnHover-DcG81d-b.js";import"./Box-DPTDcbim.js";import"./testIds-Bl6124vV.js";import"./ThemeWrapper-Bqi56xwP.js";import"./Typography-ClegP6xv.js";import"./Typography-C8COxrag.js";import"./index-D1_ZHIBm.js";import"./index-CpmRWZ9e.js";import"./GlobalStyles-BkdV5ugX.js";import"./Avatar-DAk9YqhF.js";import"./Tooltip-CNbpjzJ1.js";import"./Stack-B_sc2yyH.js";import"./styled-DlwMyq9x.js";import"./TransitionGroupContext-HZ0X4aoe.js";import"./inheritsLoose-B7h9gheN.js";import"./DatePicker-CbhqrcRL.js";import"./index-Beu9b4Vr.js";import"./createSvgIcon-B9tpKWAV.js";import"./ButtonBase-CdxKgBBF.js";import"./assertThisInitialized-B9jnkVVz.js";import"./useCommonOnChangeHandler-DI5Lva2a.js";import"./useFormControl-EHw8eS4L.js";import"./Paper-CXsvuMT9.js";import"./Checkbox-DbV7KlYF.js";import"./FormControlLabel-BLlKfmfO.js";import"./Chip-_XKwjvGn.js";import"./DateField-BFqi5wVH.js";import"./DateRangeField-DUC3IHSG.js";import"./Grid-BJJaesu6.js";import"./Input-D_MhUIRL.js";import"./TextArea-BtoFdhNv.js";import"./RadioButton.types-BELa6ELL.js";var l=(t=>(t["auto-end"]="auto-end",t["auto-start"]="auto-start",t.auto="auto",t["bottom-end"]="bottom-end",t["bottom-start"]="bottom-start",t.bottom="bottom",t["left-end"]="left-end",t["left-start"]="left-start",t.left="left",t["right-end"]="right-end",t["right-start"]="right-start",t.right="right",t["top-end"]="top-end",t["top-start"]="top-start",t.top="top",t))(l||{});const nt={title:"Example/RangeFilter/RangeFilter",component:o,parameters:{layout:"centered",docs:{source:{transform:c("RangeFilter")}}},tags:["autodocs"],argTypes:{...g.onChange,filterPlacement:{options:[...Object.values(l)],control:{type:"select"}}}},r={args:{...o.defaultProps,label:"Range Filter",placeholder:"Select a range",helperText:"Helper text",min:0,max:100,step:1,minDistance:0}},e={args:{...o.defaultProps,label:"Range Filter",placeholder:"Select a range",helperText:"Helper text",min:0,max:100,step:1,minDistance:0,Component:u,filterLabel:"Contract Amount",ComponentProps:{prefix:"Dollar"}}};var a,n,i;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  // @ts-ignore
+  args: {
+    ...RangeFilter.defaultProps,
+    label: "Range Filter",
+    placeholder: "Select a range",
+    helperText: "Helper text",
+    min: 0,
+    max: 100,
+    step: 1,
+    minDistance: 0
+  }
+}`,...(i=(n=r.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var p,m,s;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  // @ts-ignore
+  args: {
+    ...RangeFilter.defaultProps,
+    label: "Range Filter",
+    placeholder: "Select a range",
+    helperText: "Helper text",
+    min: 0,
+    max: 100,
+    step: 1,
+    minDistance: 0,
+    Component: Currency,
+    filterLabel: "Contract Amount",
+    ComponentProps: {
+      prefix: "Dollar"
+    }
+  }
+}`,...(s=(m=e.parameters)==null?void 0:m.docs)==null?void 0:s.source}}};const it=["Default","CurrencyRangeFilter"];export{e as CurrencyRangeFilter,r as Default,it as __namedExportsOrder,nt as default};
---
+++ assets/RangePickerCalendar.stories-CY4uwEv8.js
@@ -0,0 +1,17 @@
+import{j as d}from"./jsx-runtime-Du8NFWEI.js";import{f}from"./index-BnUgH7jw.js";import{s as D}from"./index-B_pG5fU1.js";import{b as i}from"./Button-Cv69wKt2.js";import"./index-Dl6G-zuu.js";import"./index-DFf-bhEl.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-Bpxk95mn.js";import"./createTheme-DtgTEXL7.js";import"./Icon.types-Ba5UCeQa.js";import"./Typography-BGHga1vv.js";import"./index-_OcG2YHj.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./palette-CiKahY_l.js";import"./useOnHover-DcG81d-b.js";import"./Box-DPTDcbim.js";import"./testIds-Bl6124vV.js";import"./ThemeWrapper-Bqi56xwP.js";import"./Typography-ClegP6xv.js";import"./Typography-C8COxrag.js";import"./index-D1_ZHIBm.js";import"./index-CpmRWZ9e.js";import"./GlobalStyles-BkdV5ugX.js";import"./Avatar-DAk9YqhF.js";import"./Tooltip-CNbpjzJ1.js";import"./Stack-B_sc2yyH.js";import"./styled-DlwMyq9x.js";import"./TransitionGroupContext-HZ0X4aoe.js";import"./inheritsLoose-B7h9gheN.js";import"./DatePicker-CbhqrcRL.js";import"./index-Beu9b4Vr.js";import"./createSvgIcon-B9tpKWAV.js";import"./ButtonBase-CdxKgBBF.js";import"./assertThisInitialized-B9jnkVVz.js";import"./useCommonOnChangeHandler-DI5Lva2a.js";import"./useFormControl-EHw8eS4L.js";import"./Paper-CXsvuMT9.js";import"./Checkbox-DbV7KlYF.js";import"./FormControlLabel-BLlKfmfO.js";import"./Chip-_XKwjvGn.js";import"./DateField-BFqi5wVH.js";import"./DateRangeField-DUC3IHSG.js";import"./Grid-BJJaesu6.js";import"./Input-D_MhUIRL.js";import"./TextArea-BtoFdhNv.js";import"./RadioButton.types-BELa6ELL.js";const pe={title:"Example/Date Picker/RangePickerCalendar",component:i,parameters:{layout:"centered",docs:{source:{transform:D("RangePickerCalendar")}}},tags:["autodocs"],argTypes:{onSelect:{action:"onChange"}},render:e=>{let t={};if(e!=null&&e.currentDate&&(t={...t,currentDate:new Date(e==null?void 0:e.currentDate)}),e!=null&&e.value&&Array.isArray(e==null?void 0:e.value)){const u=e==null?void 0:e.value.map(c=>new Date(c));t={...t,value:u}}return d.jsx(i,{...e,...t})}},r={args:{...i.defaultProps,disableFuture:!1,disableCurrent:!1,disablePast:!1,currentDate:new Date,numberOfMonths:2,onSelect:f()}},o={args:{...r.args,tools:!0,previewSelection:!0}};var m,a,p;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    ...RangePickerCalendar.defaultProps,
+    disableFuture: false,
+    disableCurrent: false,
+    disablePast: false,
+    currentDate: new Date(),
+    numberOfMonths: 2,
+    onSelect: fn()
+  }
+}`,...(p=(a=r.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};var n,s,l;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    tools: true,
+    previewSelection: true
+  }
+}`,...(l=(s=o.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};const ne=["Default","WithTools"];export{r as Default,o as WithTools,ne as __namedExportsOrder,pe as default};

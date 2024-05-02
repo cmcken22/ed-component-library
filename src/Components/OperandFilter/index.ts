8def3957a00e3a67eb8d5412c365a232d34f1db6
Conner McKenna <conner.mckenna@lockdocs.com>
2024-05-01 23:07:31 -0500

    feat: exporting operand filter

---
+++ src/Components/OperandFilter/index.ts
@@ -0,0 +1,9 @@
+export { default as OperandFilter } from "./OperandFilter";
+export type { OperandFilterProps } from "./OperandFilter";
+export {
+  OPERAND,
+  default as OperandFilterControl,
+} from "./OperandFilterControl";
+export type { OperandFilterControlProps } from "./OperandFilterControl";
+export { default as OperandFilterModal } from "./OperandFilterModal";
+export type { OperandFilterModalProps } from "./OperandFilterModal";

a5f706cc7e33fbf049b3144d53753993e2f6d327
Conner McKenna <conner.mckenna@lockdocs.com>
2024-05-09 06:50:33 -0500

    feat: updating modal stories source code

--- src/Components/Modal/Modal/Modal.stories.tsx
+++ src/Components/Modal/Modal/Modal.stories.tsx
@@ -1,8 +1,9 @@
 import { Box } from "@mui/material";
 import type { Meta, StoryObj } from "@storybook/react";
 import { useState } from "react";
-import { sourceCodeFormatter } from "sb-utils/index";
-import { Typography } from "src/index";
+import { formatArgs, sourceCodeFormatter } from "sb-utils/index";
+import { IconVariant } from "src/Components/Icon";
+import { Icon, Typography } from "src/index";
 import Button from "../../Button";
 import Modal from "./";
 
@@ -16,22 +17,77 @@ const TestModal = ({ children, ...args }: any) => {
           title="Title"
           subtitle="Header Subtitle or Short Sentence"
         />
-        <Modal.Content>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            {children}
-          </Box>
-        </Modal.Content>
+        <Modal.Content>{children}</Modal.Content>
         <Modal.Footer
           acceptBtnText="Confirm"
           cancelBtnText="Close"
           onAccept={() => setOpen(false)}
           onCancel={() => setOpen(false)}
         />
+      </Modal>
+      <Button onClick={() => setOpen(true)}>Open Modal</Button>
+    </>
+  );
+};
+
+const CustomHeaderModal = ({ children, ...args }: any) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Modal {...args} open={open} onClose={() => setOpen(false)}>
+        <Modal.Header>
+          <Box
+            display="flex"
+            alignItems="center"
+            flexDirection="row"
+            gap={1}
+            sx={{
+              color: "primary.main",
+            }}
+          >
+            <Icon icon={IconVariant.HappyEmoji} color="inherit" />
+            <Typography variant="h4" color="inherit">
+              Custom Header
+            </Typography>
+            <Icon icon={IconVariant.HappyEmoji} color="inherit" />
+          </Box>
+        </Modal.Header>
+        <Modal.Content>{children}</Modal.Content>
+        <Modal.Footer
+          acceptBtnText="Confirm"
+          cancelBtnText="Close"
+          onAccept={() => setOpen(false)}
+          onCancel={() => setOpen(false)}
+        />
+      </Modal>
+      <Button onClick={() => setOpen(true)}>Open Modal</Button>
+    </>
+  );
+};
+
+const CustomFooterModal = ({ children, ...args }: any) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Modal {...args} open={open} onClose={() => setOpen(false)}>
+        <Modal.Header
+          title="Title"
+          subtitle="Header Subtitle or Short Sentence"
+        />
+        <Modal.Content>{children}</Modal.Content>
+        <Modal.Footer
+          sx={{
+            justifyContent: "space-between",
+          }}
+        >
+          <Icon icon={IconVariant.Hand} color="inherit" />
+          <Typography variant="h4" color="inherit">
+            Custom Footer
+          </Typography>
+          <Icon icon={IconVariant.Save} color="inherit" />
+        </Modal.Footer>
       </Modal>
       <Button onClick={() => setOpen(true)}>Open Modal</Button>
     </>
@@ -72,6 +128,32 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  parameters: {
+    layout: "centered",
+    docs: {
+      source: {
+        transform: (_: any, context: any) => {
+          const { args } = context;
+          const argString = formatArgs({ args, indent: 1 });
+          return `
+<Modal
+${argString}>
+  <Modal.Header title="Title" subtitle="Header Subtitle or Short Sentence" />
+  <Modal.Content>
+      <Typography variant="bodyR">Content</Typography>
+  </Modal.Content>
+  <Modal.Footer
+    acceptBtnText="Confirm"
+    cancelBtnText="Close"
+    onAccept={() => {}}
+    onCancel={() => {}}
+  />
+</Modal>
+`;
+        },
+      },
+    },
+  },
   render: (args: any) => (
     <TestModal {...args}>
       <Typography variant="bodyR">Content</Typography>
@@ -84,14 +166,157 @@ export const Default: Story = {
 };
 
 export const MaxContent: Story = {
-  render: (args: any) => (
-    <TestModal {...args}>
-      {Array.from({ length: 100 }, (_, i) => (
+  parameters: {
+    layout: "centered",
+    docs: {
+      source: {
+        transform: (_: any, context: any) => {
+          const { args } = context;
+          const argString = formatArgs({ args, indent: 1 });
+          return `
+<Modal
+${argString}>
+  <Modal.Header title="Title" subtitle="Header Subtitle or Short Sentence" />
+  <Modal.Content>
+    {Array.from({ length: 100 }, (_, i) => (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
         <Typography key={i} variant="bodyR">
           Content
         </Typography>
+      </Box>
+    ))}
+  </Modal.Content>
+  <Modal.Footer
+    acceptBtnText="Confirm"
+    cancelBtnText="Close"
+    onAccept={() => {}}
+    onCancel={() => {}}
+  />
+</Modal>
+`;
+        },
+      },
+    },
+  },
+  render: (args: any) => (
+    <TestModal {...args}>
+      {Array.from({ length: 100 }, (_, i) => (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Typography key={i} variant="bodyR">
+            Content
+          </Typography>
+        </Box>
       ))}
     </TestModal>
+  ),
+  args: {
+    ...Default.args,
+  },
+};
+
+export const CustomHeader: Story = {
+  parameters: {
+    layout: "centered",
+    docs: {
+      source: {
+        transform: (_: any, context: any) => {
+          const { args } = context;
+          const argString = formatArgs({ args, indent: 1 });
+          return `
+<Modal
+${argString}>
+  <Modal.Header>
+    <Box
+      display="flex"
+      alignItems="center"
+      flexDirection="row"
+      gap={1}
+      sx={{
+        color: "primary.main",
+      }}
+    >
+      <Icon icon={IconVariant.HappyEmoji} color="inherit" />
+      <Typography variant="h4" color="inherit">
+        Custom Header
+      </Typography>
+      <Icon icon={IconVariant.HappyEmoji} color="inherit" />
+    </Box>
+  </Modal.Header>
+  <Modal.Content>
+    <Typography variant="bodyR">
+      Content
+    </Typography>
+  </Modal.Content>
+  <Modal.Footer
+    acceptBtnText="Confirm"
+    cancelBtnText="Close"
+    onAccept={() => {}}
+    onCancel={() => {}}
+  />
+</Modal>
+`;
+        },
+      },
+    },
+  },
+  render: (args: any) => (
+    <CustomHeaderModal {...args}>
+      <Typography variant="bodyR">Content</Typography>
+    </CustomHeaderModal>
+  ),
+  args: {
+    ...Default.args,
+  },
+};
+
+export const CustomFooter: Story = {
+  parameters: {
+    layout: "centered",
+    docs: {
+      source: {
+        transform: (_: any, context: any) => {
+          const { args } = context;
+          const argString = formatArgs({ args, indent: 1 });
+          return `
+<Modal
+${argString}>
+  <Modal.Header title="Title" subtitle="Header Subtitle or Short Sentence" />
+  <Modal.Content>
+    <Typography variant="bodyR">
+      Content
+    </Typography>
+  </Modal.Content>
+  <Modal.Footer
+    sx={{
+      justifyContent: "space-between",
+    }}
+  >
+    <Icon icon={IconVariant.Hand} color="inherit" />
+    <Typography variant="h4" color="inherit">
+      Custom Footer
+    </Typography>
+    <Icon icon={IconVariant.Save} color="inherit" />
+  </Modal.Footer>
+</Modal>
+`;
+        },
+      },
+    },
+  },
+  render: (args: any) => (
+    <CustomFooterModal {...args}>
+      <Typography variant="bodyR">Content</Typography>
+    </CustomFooterModal>
   ),
   args: {
     ...Default.args,
--- utils/sb-utils/index.tsx
+++ utils/sb-utils/index.tsx
@@ -2,38 +2,42 @@ const ind = (num: number) => {
   return "  ".repeat(num);
 };
 
+export const formatArgs = ({ args, map, indent }: any) => {
+  let res = "";
+  for (const key in args) {
+    const value = args[key];
+    if (map && map[key]) {
+      const formattedValue =
+        typeof map[key] === "function" ? map[key](value) : map[key];
+      res += `${ind(indent)}${key}={${formattedValue}}\n`;
+      continue;
+    }
+    if (typeof value === "function") {
+      res += `${ind(indent)}${key}={() => {}}\n`;
+    } else if (typeof value === "object") {
+      if (value instanceof Date) {
+        res += `${ind(indent)}${key}={new Date("${value.toISOString()}")}\n`;
+        continue;
+      }
+      const formattedValue = JSON.stringify(value, null, 2).replace(
+        /\n/g,
+        "\n  "
+      );
+      res += `${ind(indent)}${key}={${formattedValue}}\n`;
+    } else if (typeof value === "boolean") {
+      res += `${ind(indent)}${key}={${value.toString()}}\n`;
+    } else if (typeof value === "number") {
+      res += `${ind(indent)}${key}={${value.toString()}}\n`;
+    } else {
+      res += `${ind(indent)}${key}="${value}"\n`;
+    }
+  }
+  return res;
+};
+
 export const sourceCodeFormatter =
   (componentName: string, options?: any) => (_, context: any) => {
     const { map, indent = 1 } = options || { map: {}, indent: 1 };
-    let args = "";
-    for (const key in context.args) {
-      const value = context.args[key];
-      if (map && map[key]) {
-        const formattedValue =
-          typeof map[key] === "function" ? map[key](value) : map[key];
-        args += `${ind(indent)}${key}={${formattedValue}}\n`;
-        continue;
-      }
-      if (typeof value === "function") {
-        args += `${ind(indent)}${key}={() => {}}\n`;
-      } else if (typeof value === "object") {
-        if (value instanceof Date) {
-          args += `${ind(indent)}${key}={new Date("${value.toISOString()}")}\n`;
-          continue;
-        }
-        const formattedValue = JSON.stringify(value, null, 2).replace(
-          /\n/g,
-          "\n  "
-        );
-        args += `${ind(indent)}${key}={${formattedValue}}\n`;
-      } else if (typeof value === "boolean") {
-        args += `${ind(indent)}${key}={${value.toString()}}\n`;
-      } else if (typeof value === "number") {
-        args += `${ind(indent)}${key}={${value.toString()}}\n`;
-      } else {
-        args += `${ind(indent)}${key}="${value}"\n`;
-      }
-    }
-
+    const args = formatArgs({ args: context.args, map, indent });
     return `<${componentName}\n` + `${args}` + `/>`;
   };
